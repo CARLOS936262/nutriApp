5ff9d25fece56a5c443191ef6d0f02de5263b040
Alexx <24308060610056@cetis61.edu.mx>
2025-11-07 18:59:27 -0700

    cambioshechos

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def base():
-    return render_template("base.html")
+@app.route('/educacion')
+def educacion():
+    return render_template('educacion.html')
 
+@app.route('/planificacion')
+def planificacion():
+    return render_template('planificacion.html')
 
-if __name__=="__main__":
+if __name__ == '__main__':
     app.run(debug=True)
